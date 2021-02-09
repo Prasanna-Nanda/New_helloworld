46e212050ee61875d1a90a99a5820cbda98f7935
Prasanna-Nanda <prasannanandakumar92@gmail.com>
2021-02-09 14:41:04 +0530

    first commit

---
+++ hello__world/src/hello__world/hello.java
@@ -0,0 +1,12 @@
+package hello__world;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Hello!!!, welcome to the fellowship");
+	}
+
+}
